753efc6da4332daf6c431b9280f0c5714d53d979
overseas500-creator <overseas500@gmail.com>
2026-04-29 16:34:56 +0300

    Increase server payload limit to 50mb

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // MongoDB Connection
 const mongoURI = process.env.MONGODB_URI;
